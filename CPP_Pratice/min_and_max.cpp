5be0cdf15a14910c4f4ae5cd3b88e7daa2a28359
elahi68 <shaikelahi68@gmail.com>
2022-02-21 13:56:33 +0530

    minmax() function usage and disadv over max_ele min_ele

--- CPP_Pratice/min_and_max.cpp
+++ CPP_Pratice/min_and_max.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 int main()
 {
-	vector<int> vc1 = {5,1,0,8,10};
+	vector<int> vc1 = {0,1,0,8,10};
 	
 	int k=3;
 	unsigned int dummy = 0xFFFFFFFF;
@@ -28,8 +28,20 @@ int main()
 
 	max = *max_element(vc1.begin(), vc1.end());
 	min = *min_element(vc1.begin(), vc1.end());
-
-	cout<< "max: " <<max <<endl;
+	
 	cout<< "min: " <<min <<endl;
+	cout<< "max: " <<max <<endl;
 	cout<< "Total: " <<max+min <<endl;
+
+	/* dont use if not linear or sorted */
+	auto result = minmax(vc1.begin(),vc1.end());
+	
+	min = *result.first;
+	max = *result.second;
+	
+	cout<< "min: " <<min <<endl;
+        cout<< "max: " <<max <<endl;
+        cout<< "Total: " <<max+min <<endl;
+
+
 }
